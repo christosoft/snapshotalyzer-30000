523039d1f294fc82d054710ac023b065a6c6fa74
christosoft <christosoft@hotmail.com>
2020-07-22 12:12:39 -0500

    Fixed Setup.py and Pipfile

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-import setuptools import setup
+from setuptools import setup
 
 setup(
     name='snapshotalyzer-3000',
     version='0.1',
     author="christopher orozco",
     author_email="christosoft@hotmail.com",
-    description="SnapshotAlyzer 30000 is a tool to manage AWS EC2 snapshots"
+    description="SnapshotAlyzer 30000 is a tool to manage AWS EC2 snapshots",
     license="GPLv3+",
     packages=['shotty'],
     url="https://github.com/christosoft/snapshotalyzer-30000",
